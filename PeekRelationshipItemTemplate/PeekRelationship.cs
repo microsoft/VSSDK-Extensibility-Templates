37ce53d231010a2a4b9a479a981a0b0268b67b33
Alex Eyler <alexey@microsoft.com>
2015-11-17 14:19:13 -0800

    Add PeekRelationship Item Template

---
+++ PeekRelationshipItemTemplate/PeekRelationship.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.Language.Intellisense;
+
+namespace $rootnamespace$
+{
+    /// <summary>
+    /// Represents a PeekRelationship matching $rootSafeItemName$. <see cref="IPeekRelationship"/>s
+    /// are unique identifiers that apply to a <see cref="IPeekableItemSourceProvider"/>.
+    /// </summary>
+    public class $rootSafeItemName$ : IPeekRelationship
+    {
+        private $rootSafeItemName$() { }
+
+        public const string RelationshipName = "$rootSafeItemName$";
+
+        public string DisplayName => $rootSafeItemName$.RelationshipName;
+
+        public string Name => $rootSafeItemName$.RelationshipName;
+
+        public static readonly $rootSafeItemName$ Instance = new $rootSafeItemName$();
+    }
+}
